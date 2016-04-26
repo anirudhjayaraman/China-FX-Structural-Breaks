59bf1b5f1e99dd93b38eab8223863a775ecd1bc0
Anirudh Jayaraman <anirudhjayaraman@users.noreply.github.com>
2016-04-26 13:01:06 +0530

    Update try_out2.r
    Mostly added comments and an elegant code to check for an install packages (such as, in this case, strucchange)

--- try_out2.r
+++ try_out2.r
@@ -1,26 +1,41 @@
-## load package and data
+## if fxregime package is absent from installed packages, download it and load it
+if(!require('fxregime')){
+  install.packages("fxregime")
+}
+## load package and data in case fxregime is present in installed packages
 library("fxregime")
+
+# load the necessary data related to exchange rates - 'FXRatesCHF'
+# this dataset treats CHF as unit currency
 data("FXRatesCHF", package = "fxregime")
+
 ## compute returns for CNY (and explanatory currencies)
 ## since China abolished fixed USD regime
 cny <- fxreturns("CNY", frequency = "daily",
                  start = as.Date("2005-07-25"), end = as.Date("2010-02-12"),
                  other = c("USD", "JPY", "EUR", "GBP"))
+
 ## compute all segmented regression with minimal segment size of
 ## h = 100 and maximal number of breaks = 10
-
 regx <- fxregimes(CNY ~ USD + JPY + EUR + GBP,
                  data = cny, h = 100, breaks = 10, ic = "BIC")
+
+## Print summary of regression results
 summary(regx)
+
 ## minimum BIC is attained for 2-segment (1-break) model
 plot(regx)
 round(coef(regx), digits = 3)
 sqrt(coef(regx)[, "(Variance)"])
+
 ## inspect associated confidence intervals
-conf_int <- confint(regx, level = 0.9)
-breakdates(conf_int)
+cit <- confint(regx, level = 0.9)
+cit
+breakdates(cit)
+
 ## plot LM statistics along with confidence interval
 flm <- fxlm(CNY ~ USD + JPY + EUR + GBP, data = cny)
 scus <- gefp(flm, fit = NULL)
 plot(scus, functional = supLM(0.1))
-lines(conf_int)
+## add lines related to breaks to your plot
+lines(cit)
